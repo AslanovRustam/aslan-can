9b1232cf8390c9dac4c522dc3fedc9cb7790e127
AslanovRustam <aslanovrg@gmail.com>
2025-05-04 17:56:59 +0300

    remove unused code

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -9,9 +9,7 @@ const Loader = dynamic(
   { ssr: false },
 );
 
-type Props = {};
-
-export default function ViewCanvas({}: Props) {
+export default function ViewCanvas() {
   return (
     <>
       <Canvas
